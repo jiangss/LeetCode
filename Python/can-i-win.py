ca13920a2e0d5cad8b8d784c9f3c03a40a99caf3
Frost Square <jiangss@users.noreply.github.com>
2017-08-18 22:34:50 -0700

    Update can-i-win.py

--- Python/can-i-win.py
+++ Python/can-i-win.py
@@ -31,6 +31,14 @@
 # The second player will win by choosing 10 and get a total = 11, which is >= desiredTotal.
 # Same with other integers chosen by the first player, the second player will always win.
 
+# 这道题给了我们一堆数字，然后两个人，每人每次选一个数字，看数字总数谁先到给定值，有点像之前那道Nim Game，但是比那题难度大。
+# 我刚开始想肯定说用递归啊，结果写完发现TLE了，后来发现我们必须要优化效率，使用哈希表来记录已经计算过的结果。
+# 我们首先来看如果给定的数字范围大于等于目标值的话，直接返回true。如果给定的数字总和小于目标值的话，说明谁也没法赢，返回false。
+# 然后我们进入递归函数，首先我们查找当前情况是否在哈希表中存在，有的话直接返回即可。我们使用一个整型数按位来记录数组中的某个数字是否使用过，
+# 我们遍历所有数字，将该数字对应的mask算出来，如果其和used相与为0的话，说明该数字没有使用过，我们看如果此时的目标值小于等于当前数字，
+# 说明已经赢了，或者我们调用递归函数，如果返回false，说明也是第一个人赢了。为啥呢，因为当前我们已经选过数字了，此时就该对第二个人调用递归函数，
+# 只有他的结果是false，我们才能赢，所以此时我们标记true，返回true。如果遍历完所有数字，我们标记false，返回false.
+
 # Memoization solution.
 class Solution(object):
     def canIWin(self, maxChoosableInteger, desiredTotal):
